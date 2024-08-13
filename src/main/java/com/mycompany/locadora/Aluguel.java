774f219df2a96bf718ef971dd62f1f8bbff60f7b
João Igor <igor.joao@academico.ifpb.edu.br>
2024-08-13 17:00:41 -0300

    refactor: refatorar as classes Aluguel e Cliente seguindo os padrões grasp

--- src/main/java/com/mycompany/locadora/Aluguel.java
+++ src/main/java/com/mycompany/locadora/Aluguel.java
@@ -24,5 +24,36 @@ public class Aluguel {
     public int getDiasAlugada() {
         return diasAlugada;
     }
+
+    public double calcularValor() {
+        double valorCorrente = 0.0;
+        switch(fita.getCódigoDePreço()) {
+            case Fita.NORMAL:
+                valorCorrente += 2;
+                if(diasAlugada > 2) {
+                    valorCorrente += (diasAlugada - 2) * 1.5;
+                }
+                break;
+            case Fita.LANÇAMENTO:
+                valorCorrente += diasAlugada * 3;
+                break;
+            case Fita.INFANTIL:
+                valorCorrente += 1.5;
+                if(diasAlugada > 3) {
+                    valorCorrente += (diasAlugada - 3) * 1.5;
+                }
+                break;
+        }
+        return valorCorrente;
+    }
+
+    public int calcularPontosDeAlugadorFrequente() {
+        int pontos = 1;
+        if(fita.getCódigoDePreço() == Fita.LANÇAMENTO && diasAlugada > 1) {
+            pontos++;
+        }
+        return pontos;
+    }
 }
+
 
--- src/main/java/com/mycompany/locadora/Cliente.java
+++ src/main/java/com/mycompany/locadora/Cliente.java
@@ -12,7 +12,7 @@ import java.util.*;
 
 public class Cliente {
     private String nome;
-    private Collection fitasAlugadas = new ArrayList();
+    private Collection<Aluguel> fitasAlugadas = new ArrayList<>();
 
     public Cliente(String nome) {
         this.nome = nome;
@@ -30,51 +30,19 @@ public class Cliente {
         final String fimDeLinha = System.getProperty("line.separator");
         double valorTotal = 0.0;
         int pontosDeAlugadorFrequente = 0;
-        Iterator alugueis = fitasAlugadas.iterator();
-        String resultado = "Registro de Alugueis de " + getNome() + fimDeLinha;
+        StringBuilder resultado = new StringBuilder("Registro de Alugueis de " + getNome() + fimDeLinha);
 
-        while (alugueis.hasNext()) {
-            double valorCorrente = 0.0;
-            Aluguel cada = (Aluguel) alugueis.next();
-
-            // determina valores para cada linha
-            switch (cada.getFita().getCódigoDePreço()) {
-                case Fita.NORMAL:
-                    valorCorrente += 2;
-                    if (cada.getDiasAlugada() > 2) {
-                        valorCorrente += (cada.getDiasAlugada() - 2) * 1.5;
-                    }
-                    break;
-
-                case Fita.LANÇAMENTO:
-                    valorCorrente += cada.getDiasAlugada() * 3;
-                    break;
-
-                case Fita.INFANTIL:
-                    valorCorrente += 1.5;
-                    if (cada.getDiasAlugada() > 3) {
-                        valorCorrente += (cada.getDiasAlugada() - 3) * 1.5;
-                    }
-                    break;
-            } // switch
-
-            // trata de pontos de alugador frequente
-            pontosDeAlugadorFrequente++;
-
-            // adiciona bonus para aluguel de um lançamento por pelo menos 2 dias
-            if (cada.getFita().getCódigoDePreço() == Fita.LANÇAMENTO && cada.getDiasAlugada() > 1) {
-                pontosDeAlugadorFrequente++;
-            }
-
-            // mostra valores para este aluguel
-            resultado += "\t" + cada.getFita().getTítulo() + "\t" + valorCorrente + fimDeLinha;
+        for (Aluguel aluguel : fitasAlugadas) {
+            double valorCorrente = aluguel.calcularValor();
+            pontosDeAlugadorFrequente += aluguel.calcularPontosDeAlugadorFrequente();
+            resultado.append("\t").append(aluguel.getFita().getTítulo()).append("\t").append(valorCorrente).append(fimDeLinha);
             valorTotal += valorCorrente;
-        } // while
+        }
 
-        // adiciona rodapé
-        resultado += "Valor total devido: " + valorTotal + fimDeLinha;
-        resultado += "Voce acumulou " + pontosDeAlugadorFrequente + " pontos de alugador frequente";
+        resultado.append("Valor total devido: ").append(valorTotal).append(fimDeLinha);
+        resultado.append("Você acumulou ").append(pontosDeAlugadorFrequente).append(" pontos de alugador frequente");
 
-        return resultado;
+        return resultado.toString();
     }
 }
+
